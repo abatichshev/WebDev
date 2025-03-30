f5b6e914aa13fdd6bea26e872cc3b30cc0620e7f
abatichshev <156772425+abatichshev@users.noreply.github.com>
2025-03-30 15:50:30 +0500

    add lab8

---
+++ lab7.1/student_tracker/grades/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+import json
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    scores = models.JSONField(default = list)
+
+    def get_average_score(self):
+        if not self.scores:
+            return 0
+        return sum(self.scores)/len(self.scores)
+
+    def get_top_score(self):
+        return max(self.scores, default=0)
+
+    def __str__(self):
+        return self.name
